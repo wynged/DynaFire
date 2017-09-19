8bacf1605db623bda8aeee33676827cca53084ba
Eric Rudisaile <eric.rudisaile@gmail.com>
2017-09-18 20:36:52 -0400

    install bat file and xml file fixes

---
+++ deploy/Install.bat
@@ -0,0 +1,9 @@
+del /q "C:\Program Files\Dynamo\Dynamo Core\1.3\viewExtensions\DynaFire_ViewExtensionDefinition.xml"
+del /q "C:\Program Files\Dynamo\Dynamo Core\1.3\viewExtensions\DynaFire_bin\"
+
+xcopy /y /q "%~dp0DynaFire_ViewExtensionDefinition.xml" "C:\Program Files\Dynamo\Dynamo Core\1.3\viewExtensions"
+xcopy /s /q "%~dp0DynaFire_bin" "C:\Program Files\Dynamo\Dynamo Core\1.3\viewExtensions\DynaFire_bin\"
+
+xcopy /y /q "%~dp0shortcuts.txt" "C:\Program Files\Dynamo\Dynamo Core\1.3\viewExtensions\DynaFire_bin\"
+
+pause
