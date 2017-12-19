0ed1f04fae39ee00d88111de4999935c0b714bfe
Eric Rudisaile <erudisaile@microdesk.com>
2017-12-19 16:00:01 -0500

    some small tweaks

--- src/DynaFire/DynaFireExtension.cs
+++ src/DynaFire/DynaFireExtension.cs
@@ -218,7 +218,7 @@ namespace DynaFire
                 {
                     if (!shortcut.Keys.Equals(""))
                     {
-                        string shcut = shortcut.Keys + "|" + shortcut.NodeName + "|";
+                        string shcut = shortcut.Keys.ToUpper() + "|" + shortcut.NodeName + "|";
                         shortcutsFile.WriteLine(shcut);
                     }
                 }
